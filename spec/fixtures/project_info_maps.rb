d32133502a707429e9ef020108359383a7f95618
Victor <victory1162@gmail.com>
2024-10-19 17:54:23 +0800

    feat: Maps spec/fixtures

--- spec/fixtures/project_info_maps.rb
+++ spec/fixtures/project_info_maps.rb
@@ -3,7 +3,7 @@
 require 'http'
 require 'yaml'
 
-def text_search(config, text_query)
+text_search = lambda do |config, text_query|
   url = 'https://places.googleapis.com/v1/places:searchText'
   HTTP.headers(
     'X-Goog-Api-Key' => config['GOOGLE_MAPS_KEY'],
@@ -18,7 +18,7 @@ maps_results = {}
 
 ## HAPPY project request
 text_query = 'NTHU'
-maps_response[text_query] = text_search(config, text_query)
+maps_response[text_query] = text_search.call(config, text_query)
 place = maps_response[text_query].parse
 
 maps_results['id'] = place['places'][0]['id']
@@ -26,12 +26,18 @@ maps_results['id'] = place['places'][0]['id']
 maps_results['formatted_address'] = place['places'][0]['formattedAddress']
 # should be 300新竹市東區光復路二段101號
 
-maps_results['display_name'] = place['places'][0]['displayName']
+maps_results['display_name'] = place['places'][0]['displayName']['text']
 # should be 清華大學
 
-maps_results['rating'] = place['places'][0]['rating']
+maps_results['rating'] = place['places'][0]['rating'].to_f
 # should be 4.6
 
-maps_results['reviews'] = place['places'][0]['reviews']
+maps_results['reviews'] = place['places'][0]['reviews'].map do |review|
+  {
+    author_name: review['authorAttribution']['displayName'],
+    rating: review['rating'].to_f,
+    text: review['originalText']['text']
+  }
+end
 
 File.write('spec/fixtures/maps_results.yml', maps_results.to_yaml)
