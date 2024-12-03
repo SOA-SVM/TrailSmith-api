f643ab23b5f1eb925ea4a9156df3c7b20ce9b846
yuhsiangl <aalex910606@gmail.com>
2024-12-03 21:01:53 +0800

    test: add test for distance matrix api

---
+++ spec/tests/integration/gateway_distance_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative '../../helpers/spec_helper'
+require_relative '../../lib/distance_api'
+
+describe 'Tests Distance Matrix API library' do
+  describe 'Distance information' do
+    it 'HAPPY: should provide correct project attributes' do
+      way = TrailSmith::DistanceApi.new(GOOGLE_MAPS_KEY)
+        .way(STARTING_SPOT, NEXT_SPOT, TRAVEL_MODE)
+      _(way.travel_time).must_equal DISTANCE_CORRECT['travel_time']['sec']
+      _(way.distance).must_equal DISTANCE_CORRECT['distance']['meter']
+    end
+
+    it 'SAD: should raise exception when unauthorized' do
+      _(proc do
+        TrailSmith::DistanceApi.new('BAD_TOKEN').way(STARTING_SPOT, NEXT_SPOT, TRAVEL_MODE)
+      end).must_raise TrailSmith::DistanceApi::Response::Forbidden
+    end
+
+    it 'SAD: should raise exception on invalid travel mode' do
+      _(proc do
+        TrailSmith::DistanceApi.new('BAD_TOKEN').way(STARTING_SPOT, NEXT_SPOT, 'hi')
+      end).must_raise TrailSmith::DistanceApi::Response::Invalid
+    end
+
+    it 'SAD: should raise exception on incorrect spot name' do
+      _(proc do
+        TrailSmith::DistanceApi.new('BAD_TOKEN').way('no this spot', NEXT_SPOT, TRAVEL_MODE)
+      end).must_raise TrailSmith::DistanceApi::Response::NotFound
+
+      _(proc do
+        TrailSmith::DistanceApi.new('BAD_TOKEN').way(STARTING_SPOT, 'no this spot', TRAVEL_MODE)
+      end).must_raise TrailSmith::DistanceApi::Response::NotFound
+
+      _(proc do
+        TrailSmith::DistanceApi.new('BAD_TOKEN').way('no this spot', 'no this spot', TRAVEL_MODE)
+      end).must_raise TrailSmith::DistanceApi::Response::NotFound
+    end
+  end
+end
