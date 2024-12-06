0b1e5e9415e0fda312a4c4c093cc389a3ebd6e10
yuhsiangl <aalex910606@gmail.com>
2024-12-06 10:46:22 +0800

    refactor: refactor way value object

--- spec/tests/integration/gateway_distance_spec.rb
+++ spec/tests/integration/gateway_distance_spec.rb
@@ -2,7 +2,6 @@
 
 require_relative '../../helpers/spec_helper'
 require_relative '../../helpers/vcr_helper'
-require_relative '../../lib/distance_api'
 
 describe 'Tests Distance Matrix API library' do
   VcrHelper.setup_vcr
@@ -16,36 +15,28 @@ describe 'Tests Distance Matrix API library' do
   end
   describe 'Distance information' do
     it 'HAPPY: should provide correct project attributes' do
-      way = TrailSmith::DistanceApi.new(GOOGLE_MAPS_KEY)
-        .way(STARTING_SPOT, NEXT_SPOT, TRAVEL_MODE)
+      way = TrailSmith::Distance::WayMapper.new(GOOGLE_MAPS_KEY)
+        .find(STARTING_SPOT, NEXT_SPOT, TRAVEL_MODE)
       _(way.travel_time).must_equal DISTANCE_CORRECT['travel_time']['sec']
       _(way.distance).must_equal DISTANCE_CORRECT['distance']['meter']
     end
 
     it 'SAD: should raise exception when unauthorized' do
       _(proc do
-        TrailSmith::DistanceApi.new('BAD_TOKEN').way(STARTING_SPOT, NEXT_SPOT, TRAVEL_MODE)
-      end).must_raise TrailSmith::DistanceApi::Response::Forbidden
+        TrailSmith::Distance::WayMapper.new('BAD_TOKEN').find(STARTING_SPOT, NEXT_SPOT, TRAVEL_MODE)
+      end).must_raise TrailSmith::Distance::Api::Response::Forbidden
     end
 
     it 'SAD: should raise exception on invalid travel mode' do
       _(proc do
-        TrailSmith::DistanceApi.new('BAD_TOKEN').way(STARTING_SPOT, NEXT_SPOT, 'hi')
-      end).must_raise TrailSmith::DistanceApi::Response::InvalidRequest
+        TrailSmith::Distance::WayMapper.new('BAD_TOKEN').find(STARTING_SPOT, NEXT_SPOT, 'hi')
+      end).must_raise TrailSmith::Distance::Api::Response::InvalidRequest
     end
 
     it 'SAD: should raise exception on incorrect spot name' do
       _(proc do
-        TrailSmith::DistanceApi.new('BAD_TOKEN').way('no this spot', NEXT_SPOT, TRAVEL_MODE)
-      end).must_raise TrailSmith::DistanceApi::Response::NotFound
-
-      _(proc do
-        TrailSmith::DistanceApi.new('BAD_TOKEN').way(STARTING_SPOT, 'no this spot', TRAVEL_MODE)
-      end).must_raise TrailSmith::DistanceApi::Response::NotFound
-
-      _(proc do
-        TrailSmith::DistanceApi.new('BAD_TOKEN').way('no this spot', 'no this spot', TRAVEL_MODE)
-      end).must_raise TrailSmith::DistanceApi::Response::NotFound
+        TrailSmith::Distance::WayMapper.new('BAD_TOKEN').find('no this spot', NEXT_SPOT, TRAVEL_MODE)
+      end).must_raise TrailSmith::Distance::Api::Response::NotFound
     end
   end
 end
